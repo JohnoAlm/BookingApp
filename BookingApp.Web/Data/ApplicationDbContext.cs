7d14219f5fa484eb3af889883b0967c81fb1f4ce
DZM77 <dimitris.bjorlingh@outlook.com>
2022-08-22 13:32:20 +0200

    Entities

--- BookingApp.Web/Data/ApplicationDbContext.cs
+++ BookingApp.Web/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BookingApp.Core.Entitis;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookingApp.Web.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<GymClass> GymClasses => Set<GymClass>();
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
