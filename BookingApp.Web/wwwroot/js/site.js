5fbaf48dc09d15340bfe0c0d2e61744bbb1ca493
DZM77 <dimitris.bjorlingh@outlook.com>
2022-08-24 13:29:23 +0200

    FrontEndValidation

--- BookingApp.Web/wwwroot/js/site.js
+++ BookingApp.Web/wwwroot/js/site.js
@@ -10,3 +10,8 @@ function failCreate(response) {
     console.log(response, 'failed to create');
     createAjax.innerHTML = response.responseText;
 }
+
+function fixValidation() {
+    const form = createAjax.querySelector('form');
+    $.validator.unobtrusive.parse(form);
+}
