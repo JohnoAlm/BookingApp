926ea949233d35aea6b511c38bf5f4c518ff94dc
DZM77 <dimitris.bjorlingh@outlook.com>
2022-08-29 13:31:36 +0200

    Add Authorize on Create

--- BookingApp.Web/Controllers/GymClassesController.cs
+++ BookingApp.Web/Controllers/GymClassesController.cs
@@ -130,6 +130,7 @@ namespace BookingApp.Web.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles ="Admin")]
         public async Task<IActionResult> Create([Bind("Id,Name,StartDate,Duration,Description")] GymClass gymClass)
         {
             if (ModelState.IsValid)
